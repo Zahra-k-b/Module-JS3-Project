2c6299329478f48e139bfec1e51847d09f3725ca
Zahra <zahrakhademi420@gmail.com>
2024-01-16 23:05:11 +0000

    create element

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  //console.log(allEpisodes);
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerhtml=""; 
+  episodeList.forEach((episode) => {
+    // console.log(episode.name);
+    console.log(episode.summery);
+    const titleElem = document.createElement("h2");
+    titleElem.textContent = `${episode.name}`
+    rootElem.appendChild(titleElem);
+    const imageElem = document.createElement("img");
+    imageElem.setAttribute("src", episode.image.medium);
+    rootElem.appendChild(imageElem);
+    // const descriptionElm = document.createElement("p");
+    // descriptionElm.textContent = `${episode.summery}`;
+    // rootElem.appendChild(descriptionElm);
+    const descriptionElm = document.createElement("p");
+    descriptionElm.textContent = `${episode.summary}`;
+    rootElem.appendChild(descriptionElm);
+  });
 }
 
-window.onload = setup;
+// episodeList.forEach((episode) => {
+//   const episodeList = document.createElement("div");
+//   episodeElem = classList.add("episode");
+
+//   const titleElem = document.createElement("h2");
+//   titleElem.textContent = `${episode.name} - S${episode.season}E${episode.number}`
+// });
+
+window.onload = setup();
+
