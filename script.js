785e1abf79e2c42273de2bb630aaced227728b2e
Zahra <zahrakhademi420@gmail.com>
2024-01-30 22:25:44 +0000

    level 100

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
 //You can edit ALL of the code here
+
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   //console.log(allEpisodes);
-  makePageForEpisodes(allEpisodes);
+  makePageForEpisode(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.innerhtml=""; 
-  episodeList.forEach((episode) => {
-    // console.log(episode.name);
-    console.log(episode.summery);
-    const titleElem = document.createElement("h2");
-    titleElem.textContent = `${episode.name}`
-    rootElem.appendChild(titleElem);
-    const imageElem = document.createElement("img");
-    imageElem.setAttribute("src", episode.image.medium);
-    rootElem.appendChild(imageElem);
-    // const descriptionElm = document.createElement("p");
-    // descriptionElm.textContent = `${episode.summery}`;
-    // rootElem.appendChild(descriptionElm);
-    const descriptionElm = document.createElement("p");
-    descriptionElm.textContent = `${episode.summary}`;
-    rootElem.appendChild(descriptionElm);
+
+function createFilmCard(template, episode) {
+const cardNode = template.content.cloneNode(true);
+cardNode.querySelector("h3").textContent = episode.name;
+cardNode.querySelector("img").src = episode.image.medium;
+cardNode.querySelector("p").innerHTML = episode.summary;
+return cardNode;
+}
+
+function makePageForEpisode(allEpisodes) {
+  
+  const rootElm = document.getElementById("root");
+  const template = document.getElementById("film-card");
+
+  allEpisodes.forEach((episode) => {
+    // create card is the completing template
+    const card = createFilmCard(template, episode);
+    // add to page
+    rootElm.appendChild(card);
   });
+
 }
 
-// episodeList.forEach((episode) => {
-//   const episodeList = document.createElement("div");
-//   episodeElem = classList.add("episode");
 
-//   const titleElem = document.createElement("h2");
-//   titleElem.textContent = `${episode.name} - S${episode.season}E${episode.number}`
-// });
-
-window.onload = setup();
+window.onload = setup;
 
